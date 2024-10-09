8f221b38523da00e064a2b39f6b0897682f7b33e
jsawyer324 <jsawyer324@gmail.com>
2024-10-09 12:21:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 sudo sgdisk -n 1::+512M /dev/vda -t 1:ef00
 sudo sgdisk -n 2:: /dev/vda 
 
+sleep 10
+
 #mount
 sudo mkfs.fat -F 32 /dev/vda1
 sudo fatlabel /dev/vda1 NIXBOOT
@@ -10,12 +12,18 @@ sudo mount /dev/disk/by-label/NIXROOT /mnt
 sudo mkdir -p /mnt/boot
 sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot
 
+sleep 10
+
 #copy
 sudo cp ./configuration.nix /mnt/etc/nixos/
-sudo cp us5068.nordvpn.com.udp1194.ovpn /mnt/etc/nixos/
+sudo cp ./us5068.nordvpn.com.udp1194.ovpn /mnt/etc/nixos/
+
+sleep 10
 
 #config
 sudo nixos-generate-config --root /mnt
+
+sleep 10
 
 #install
 cd /mnt
