8a2fc368b436ec7cbc152336894cd0e40ac91f0c
jsawyer324 <jsawyer324@gmail.com>
2024-10-10 12:24:11 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports =
-    [
+  imports = [
 	./hardware-configuration.nix
-    ];
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -27,21 +26,21 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+	LC_ADDRESS = "en_US.UTF-8";
+	LC_IDENTIFICATION = "en_US.UTF-8";
+	LC_MEASUREMENT = "en_US.UTF-8";
+	LC_MONETARY = "en_US.UTF-8";
+	LC_NAME = "en_US.UTF-8";
+	LC_NUMERIC = "en_US.UTF-8";
+	LC_PAPER = "en_US.UTF-8";
+	LC_TELEPHONE = "en_US.UTF-8";
+	LC_TIME = "en_US.UTF-8";
   };
 
   services.xserver = {
 	enable = true;
 	displayManager.lightdm.enable = true;
-	desktopManager.mate.enable = true;
+	desktopManager.xfce.enable = true;
 	xkb = {
     		layout = "us";
     		variant = "";
@@ -53,25 +52,23 @@
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+	enable = true;
+    	alsa.enable = true;
+    	alsa.support32Bit = true;
+    	pulse.enable = true;
   };
 
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.james = {
-    isNormalUser = true;
-    initialPassword = "password";
-    description = "james";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    	isNormalUser = true;
+    	initialPassword = "password";
+    	description = "james";
+    	extraGroups = [ "networkmanager" "wheel" ];
+    	packages = with pkgs; [
+    	    #thunderbird
+    	];
   };
-
-  # Allow unfree packages
+fight the change
   nixpkgs.config.allowUnfree = true;
 
 
