5f27f75c5ac3f1a75078c0e8de2beeae6f74d312
James Sawyer <jsawyer324@gmail.com>
2024-10-11 09:55:51 -0500

    setup add hostname update added ovpn profiles cleanup configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,22 +2,23 @@
 
 {
   imports = [
-	./hardware-configuration.nix
+		./hardware-configuration.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; 
+	networking = {
+		hostName = "nixoshost";
+		networkmanager.enable = true;
+	};
 
-  networking.networkmanager.enable = true;
-  services.networkd-dispatcher.enable = true;
   environment.etc = {
-	"NetworkManager/dispatcher.d/10-routes.sh" = {
-      		text = ''
-			ip route add 192.168.1.0/24 via 10.0.2.2
-      		'';
-		mode = "0700";
+		"NetworkManager/dispatcher.d/10-routes.sh" = {
+    	text = ''
+				ip route add 192.168.1.0/24 via 10.0.2.2
+      '';
+			mode = "0700";
     	};
   };	
 
@@ -26,24 +27,24 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-	LC_ADDRESS = "en_US.UTF-8";
-	LC_IDENTIFICATION = "en_US.UTF-8";
-	LC_MEASUREMENT = "en_US.UTF-8";
-	LC_MONETARY = "en_US.UTF-8";
-	LC_NAME = "en_US.UTF-8";
-	LC_NUMERIC = "en_US.UTF-8";
-	LC_PAPER = "en_US.UTF-8";
-	LC_TELEPHONE = "en_US.UTF-8";
-	LC_TIME = "en_US.UTF-8";
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
   };
 
   services.xserver = {
-	enable = true;
-	displayManager.lightdm.enable = true;
-	desktopManager.xfce.enable = true;
-	xkb = {
-    		layout = "us";
-    		variant = "";
+		enable = true;
+		displayManager.lightdm.enable = true;
+		desktopManager.xfce.enable = true;
+		xkb = {
+			layout = "us";
+			variant = "";
   	};
   };
 
@@ -52,10 +53,10 @@
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-	enable = true;
-    	alsa.enable = true;
-    	alsa.support32Bit = true;
-    	pulse.enable = true;
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
   };
 
 
@@ -73,14 +74,14 @@
 
 
   environment.systemPackages = with pkgs; [
-	brave
-	networkmanager-openvpn
-	nfs-utils
-	cifs-utils
-	session-desktop
-	#spice-vdagent
-	#spice-autorandr  
-	numlockx  
+		brave
+		networkmanager-openvpn
+		nfs-utils
+		cifs-utils
+		session-desktop
+		#spice-vdagent
+		#spice-autorandr  
+		numlockx  
   ];
 
    programs.git = {
@@ -92,12 +93,13 @@
    };
     
    services = {
-	openssh.enable = true;		# Enable the OpenSSH daemon.
-	spice-vdagentd.enable = true;
-	qemuGuest.enable = true;	
-    	samba.enable = true;    	# Enable the Samba daemon.
-    	gvfs.enable = true;     	# Mount, trash, and other functionalities
-    	tumbler.enable = true;  	# Thumbnail support for images
+		networkd-dispatcher.enable = true;
+		openssh.enable = true;		# Enable the OpenSSH daemon.
+		spice-vdagentd.enable = true;
+		qemuGuest.enable = true;	
+		samba.enable = true;    	# Enable the Samba daemon.
+		gvfs.enable = true;     	# Mount, trash, and other functionalities
+		tumbler.enable = true;  	# Thumbnail support for images
    };
 
 
@@ -105,26 +107,26 @@
 
   networking.firewall.extraCommands = ''
 
-	#Allow loopback device (internal communication)
-	iptables -A INPUT -i lo -j ACCEPT
-	iptables -A OUTPUT -o lo -j ACCEPT
+		#Allow loopback device (internal communication)
+		iptables -A INPUT -i lo -j ACCEPT
+		iptables -A OUTPUT -o lo -j ACCEPT
 
-	#Allow all local traffic.
-	iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
-	iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
+		#Allow all local traffic.
+		iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
+		iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
 
-	#Allow VPN establishment
-	iptables -A OUTPUT -p udp --match multiport --dports 53,1194 -j ACCEPT
-	iptables -A INPUT -p udp --match multiport --sports 53,1194 -j ACCEPT
+		#Allow VPN establishment
+		iptables -A OUTPUT -p udp --match multiport --dports 53,1194 -j ACCEPT
+		iptables -A INPUT -p udp --match multiport --sports 53,1194 -j ACCEPT
 
-	#Accept all TUN connections (tun = VPN tunnel)
-	iptables -A OUTPUT -o tun+ -j ACCEPT
-	iptables -A INPUT -i tun+ -j ACCEPT
+		#Accept all TUN connections (tun = VPN tunnel)
+		iptables -A OUTPUT -o tun+ -j ACCEPT
+		iptables -A INPUT -i tun+ -j ACCEPT
 
-	#Set default policies to drop all communication unless specifically allowed
-	iptables -P INPUT DROP
-	iptables -P OUTPUT DROP
-	iptables -P FORWARD DROP  
+		#Set default policies to drop all communication unless specifically allowed
+		iptables -P INPUT DROP
+		iptables -P OUTPUT DROP
+		iptables -P FORWARD DROP  
   '';
 
   system.stateVersion = "24.05"; # Did you read the comment?
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-#format disks
+#!/bin/bash
+
+#partition disk
 sudo sgdisk -n 1::+512M /dev/vda -t 1:ef00
 sudo sgdisk -n 2::+2G /dev/vda -t 2:8200
 sudo sgdisk -n 3:: /dev/vda 
 
-#sleep 10
-
-#mount
+#format and mount
 sudo mkfs.fat -F 32 /dev/vda1
 sudo fatlabel /dev/vda1 NIXBOOT
 sudo mkfs.ext4 /dev/vda3 -L NIXROOT
@@ -13,32 +13,23 @@ sudo mount /dev/disk/by-label/NIXROOT /mnt
 sudo mkdir -p /mnt/boot
 sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot
 
-#sleep 10
-
-#swap
-#sudo dd if=/dev/zero of=/mnt/.swapfile bs=1024 count=2097152
-#sudo chmod 600 /mnt/.swapfile
-#sudo mkswap /mnt/.swapfile
-#sudo swapon /mnt/.swapfile
+#activate swap
 sudo mkswap /dev/vda2
 sudo swapon /dev/vda2
 
-#sleep 10
-
 #config
 sudo nixos-generate-config --root /mnt
-
-#sleep 10
 
 #copy
 sudo cp ./configuration.nix /mnt/etc/nixos/
 sudo cp ./us5068.nordvpn.com.udp1194.ovpn /mnt/etc/nixos/
 
-#sleep 10
-
+#update hostname
+read -rp "Enter hostname: " HOSTNAME
+sed -i "s/nixoshost/$HOSTNAME/g" /mnt/etc/locale.gen
 
 #install
-cd /mnt
+cd /mnt || exit
 sudo nixos-install
 
 reboot now
