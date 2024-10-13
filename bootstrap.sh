76e2ef664c369841e0992949442a35c2748d36d6
James Sawyer <jsawyer324@gmail.com>
2024-10-13 11:20:52 -0500

    testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p bash git
 #! nix-shell -i bash
-#! nix-shell --command "git clone https://github.com/jsawyer324/qemu-nixos.git" 
+#! nix-shell -v 3 --command "git clone https://github.com/jsawyer324/qemu-nixos.git" 
 
-echo "Cloning qemu-nixos5"
+echo "Cloning qemu-nixos6"
 #git clone https://github.com/jsawyer324/qemu-nixos.git
 cd /home/nixos/qemu-nixos || exit
 sudo sh ./setup.sh
