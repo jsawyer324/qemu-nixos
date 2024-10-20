b3b5844028df71cf3bae07cdffc93cbac0fe9de6
James Sawyer <jsawyer324@gmail.com>
2024-10-20 11:46:22 -0500

    added comment on howto run

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env nix-shell
 
+# to run, load up the iso and run the following command
+# sh <(curl -sL bit.ly/j324nix)
+
+
 nix-shell -p git --run "git clone https://github.com/jsawyer324/qemu-nixos.git"
 
 cd /home/nixos/qemu-nixos || exit
