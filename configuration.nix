80d72d0fc9010efa3f2b20a01b90fae4201206e4
James Sawyer <jsawyer324@gmail.com>
2025-02-12 16:44:35 -0600

    update minimal config

--- configuration.nix
+++ configuration.nix
@@ -9,54 +9,54 @@
 	boot.loader.efi.canTouchEfiVariables = true;	# Allow the boot loader to modify the EFI variables.
 
   	networking = {
-		hostName = "nixoshost";
-		networkmanager.enable = true;
-		firewall = {
-			enable = false;
-			extraCommands = ''
+			hostName = "nixoshost";
+			networkmanager.enable = true;
+			# firewall = {
+			# 	enable = false;
+			# 	extraCommands = ''
 
-				#Allow loopback device (internal communication)
-				iptables -A INPUT -i lo -j ACCEPT
-				iptables -A OUTPUT -o lo -j ACCEPT
+			# 		#Allow loopback device (internal communication)
+			# 		iptables -A INPUT -i lo -j ACCEPT
+			# 		iptables -A OUTPUT -o lo -j ACCEPT
 
-				#Allow all local traffic.
-				iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
-				iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
+			# 		#Allow all local traffic.
+			# 		iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
+			# 		iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
 
-				#Allow VPN establishment
-				iptables -A OUTPUT -p udp --match multiport --dports 53,1194 -j ACCEPT
-				iptables -A INPUT -p udp --match multiport --sports 53,1194 -j ACCEPT
+			# 		#Allow VPN establishment
+			# 		iptables -A OUTPUT -p udp --match multiport --dports 53,1194 -j ACCEPT
+			# 		iptables -A INPUT -p udp --match multiport --sports 53,1194 -j ACCEPT
 
-				#Accept all TUN connections (tun = VPN tunnel)
-				iptables -A OUTPUT -o tun+ -j ACCEPT
-				iptables -A INPUT -i tun+ -j ACCEPT
+			# 		#Accept all TUN connections (tun = VPN tunnel)
+			# 		iptables -A OUTPUT -o tun+ -j ACCEPT
+			# 		iptables -A INPUT -i tun+ -j ACCEPT
 
-				#Set default policies to drop all communication unless specifically allowed
-				iptables -P INPUT DROP
-				iptables -P OUTPUT DROP
-				iptables -P FORWARD DROP  
-			'';
-		};
+			# 		#Set default policies to drop all communication unless specifically allowed
+			# 		iptables -P INPUT DROP
+			# 		iptables -P OUTPUT DROP
+			# 		iptables -P FORWARD DROP  
+			# 	'';
+			# };
 	};
 
-	environment.etc = {
-			"NetworkManager/dispatcher.d/10-routes.sh" = {
-			text = ''
-					ip route add 192.168.1.0/24 via 10.0.2.2
-		'';
-				mode = "0700";
-		};
-			"X11/xorg.conf.d/10-monitor.conf" = {
-			text = ''
-					Section "Monitor"
-					Identifier "Virtual-1"
-					Modeline "1920x1153_60.00"  184.75  1920 2048 2248 2576  1153 1156 1166 1196 -hsync +vsync
-					Option "PreferredMode" "1920x1153_60.00"
-					EndSection
-		'';
-				mode = "0660";
-		};
-  	};	
+	# environment.etc = {
+	# 		"NetworkManager/dispatcher.d/10-routes.sh" = {
+	# 		text = ''
+	# 				ip route add 192.168.1.0/24 via 10.0.2.2
+	# 	'';
+	# 			mode = "0700";
+	# 	};
+	# 		"X11/xorg.conf.d/10-monitor.conf" = {
+	# 		text = ''
+	# 				Section "Monitor"
+	# 				Identifier "Virtual-1"
+	# 				Modeline "1920x1153_60.00"  184.75  1920 2048 2248 2576  1153 1156 1166 1196 -hsync +vsync
+	# 				Option "PreferredMode" "1920x1153_60.00"
+	# 				EndSection
+	# 	'';
+	# 			mode = "0660";
+	# 	};
+  # 	};	
 
 
 	time.timeZone = "America/Chicago";
@@ -97,16 +97,16 @@
 		};
 
 		# networkd-dispatcher.enable = true;  # Enable the networkd-dispatcher service.
-		openssh.enable = true;				# Enable the OpenSSH daemon.
+		# openssh.enable = true;				# Enable the OpenSSH daemon.
 		spice-vdagentd.enable = true;		# Enable the Spice VDAgent daemon.
 		qemuGuest.enable = true;			# Enable the QEMU guest agent.
-		samba ={
-			enable = true;    			# Enable the Samba daemon.
-			enableNmbd = false;         # Disable the NetBIOS daemon.
-    	enableWinbindd = false;     # Disable the Winbind daemon.
-		};
-		gvfs.enable = true;     			# Mount, trash, and other functionalities
-		tumbler.enable = true;  			# Thumbnail support for images
+		# samba ={
+		# 	enable = true;    			# Enable the Samba daemon.
+		# 	enableNmbd = false;         # Disable the NetBIOS daemon.
+    # 	enableWinbindd = false;     # Disable the Winbind daemon.
+		# };
+		# gvfs.enable = true;     			# Mount, trash, and other functionalities
+		# tumbler.enable = true;  			# Thumbnail support for images
 		# openvpn.servers.vpn = {
 		# 	config = "config /etc/nixos/ovpn/Chicago/us8389.nordvpn.com.udp1194.ovpn";
 		# 	autoStart = false;
@@ -128,25 +128,25 @@
 		extraGroups = [ "networkmanager" "wheel" ];
 	};
 
-	nixpkgs.config.allowUnfree = true;	# Allow unfree packages.
+	# nixpkgs.config.allowUnfree = true;	# Allow unfree packages.
 
-	environment.systemPackages = with pkgs; [
-		brave
-		networkmanager-openvpn
-		nfs-utils
-		cifs-utils
-		session-desktop
-		numlockx
-		mesa
-	];
+	# environment.systemPackages = with pkgs; [
+	# 	brave
+	# 	networkmanager-openvpn
+	# 	nfs-utils
+	# 	cifs-utils
+	# 	session-desktop
+	# 	numlockx
+	# 	mesa
+	# ];
 
-	programs.git = {
-		enable = true;
-		config = {
-				user.name = "James Sawyer";
-				user.email = "jsawyer324@gmail.com";
-		};
-	};
+	# programs.git = {
+	# 	enable = true;
+	# 	config = {
+	# 			user.name = "James Sawyer";
+	# 			user.email = "jsawyer324@gmail.com";
+	# 	};
+	# };
     
   	system.stateVersion = "24.05"; # Did you read the comment?
 }
---
+++ configuration_minimal.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+		./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+	networking = {
+		hostName = "nixoshost";
+		networkmanager.enable = true;
+		firewall.enable = false;
+	};
+
+  environment.etc = {
+		
+  };	
+
+
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
+  };
+
+
+	services = {
+		
+		xserver = {
+			enable = true;
+			displayManager.lightdm.enable = true;
+			desktopManager.xfce.enable = true;
+			xkb = {
+				layout = "us";
+				variant = "";
+			};
+  	};
+
+		# pipewire = {
+		# 	enable = true;
+		# 	alsa.enable = true;
+		# 	alsa.support32Bit = true;
+		# 	pulse.enable = true;
+		# };
+
+		spice-vdagentd.enable = true;
+		qemuGuest.enable = true;	
+
+	}
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  users.users.james = {
+    	isNormalUser = true;
+    	initialPassword = "password";
+    	description = "james";
+    	extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
