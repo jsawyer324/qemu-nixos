c86869653a6dae2d50758ea4560c798f71ebb97e
jsawyer324 <jsawyer324@gmail.com>
2025-03-05 20:20:50 -0600

    Update configuration_1.nix
    added ssh

--- configuration_1.nix
+++ configuration_1.nix
@@ -98,6 +98,17 @@
 			pulse.enable = true;
 		};
 
+		openssh = {
+			enable = true;
+			ports = [ 22 ];
+			settings = {
+				PasswordAuthentication = true;
+				#AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+				#UseDns = true;
+				X11Forwarding = true;
+				PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+			};
+		};
 		spice-vdagentd.enable = true;
 		qemuGuest.enable = true;
 		networkd-dispatcher.enable = true;
